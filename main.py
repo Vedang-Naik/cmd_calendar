f3f8e078a92d72f8cafeb5bbdec81702b6c77242
Vedang Naik <vedang.a.naik@gmail.com>
2018-08-29 18:21:48 +0530

    Completed error-checking and added documentation to main.py

--- main.py
+++ main.py
@@ -1,145 +1,219 @@
-import calendar
-import colorama
-import argparse
-import shlex
-import pickle
-import os
-import datetime
-import numpy
-import time
+# ---- PROJECT DESCRIPTION ----
+# CMD_CALENDAR is an attempt to make a calendar system with events and reminders on the command line. It is akin to a very-simplified Google Calendar, Windows Calendar, etc.
+# FEATURES: 1. The calendar of any month or year can be displayed here (Gregorian only, infinitely extended in both directions).
+#           2. Events can be added to any particular day. One can search for events and delete them.
+# USAGE: Start main.py using python. At the calendar> prompt, enter the specific command to do whatever you wish to do. calendar> -h will provide help.
+# PLANS: 1. Reminders for events through either email or desktop notifications are planned. 
+#        2. A more colourful UI might be implemented.
+# 
+# WORKING: 1. Once main.py starts up, it shows the calendar for the current month and an input prompt.
+#          2. The user enters a argument at the prompt. This is error-checked and parsed by parseCommand()
+#          3. The parsed command is sent to execCommand(), which carries out the user's operation.
+#             -> If command parsing fails, the error will be displayed, followed by the next prompt.
+#             -> If the input is invalid, the error page is shown, followed by the next prompt.
+#          4. Once the operation is done, the prompt is displayed again for the next command.
+#          5. Steps 2-4 continue for ever until the user inputs "exit" or closes the program in some other fashion.
+
+import calendar # In-built: Used for getting the calendars for any month or year.
+import colorama # pip installed: Used for enabling ANSI sequences for cursor positioning and colours in Windows.
+import argparse # In-built: Used for argument parsing.
+import shlex # In-built: Extends argparse to strings which resemble arguments.
+import pickle # In-built: Used to store the events dictionary offline.
+import os # In-built: Used to get the CMD dimensions and clear the screen.
+import datetime # In-built: Used for manipulating dates easily.
+import numpy # pip installed: Used for reshaping an array in one place; it will be removed once a better way is found.
+import time # In-built: Used for the sleep function; if you do not want the main page to be displayed, you can delete this.
 
 from renderer import renderMainPage, renderMMMonth, renderYMYear, renderEventSearchBox, renderError
 
-# Modifies the default calendar list returned by Calendar.monthdatescalendar().
+# --- FUNCTION ----
+# DESC: Adds events to days and removes days not in a month for a list of dates in a month.
+# INPUT: The calendar list returned by calendar.Calendar(0).monthdatescalendar
+# RETURN: The modified calendar list if successful, which it should always be.
+# -----------------
 def addEventToMonthCalendar(monthCalendar, month):
-	global EVENTS
+    global EVENTS
+    for i in range(0, len(monthCalendar)):
+        for j in range(0, 7):
+            if monthCalendar[i][j].month != month: # If the day is not in the month, replace with None.
+                monthCalendar[i][j] = None
+            elif monthCalendar[i][j] in EVENTS.keys(): # If this day has an event, make tuple of day and event
+                monthCalendar[i][j] = (monthCalendar[i][j], len(EVENTS[monthCalendar[i][j]]))
+    return monthCalendar
 
-	for i in range(0, len(monthCalendar)):
-		for j in range(0, 7):
-			if monthCalendar[i][j].month != month: # If the day is not in the month, replace with None.
-				monthCalendar[i][j] = None
-			elif monthCalendar[i][j] in EVENTS.keys(): # If this day has an event, make tuple of day and event
-				monthCalendar[i][j] = (monthCalendar[i][j], len(EVENTS[monthCalendar[i][j]]))
-
-	return monthCalendar
-
-# Set up parsing of the command statement into its components.
+# --- FUNCTION ----
+# DESC: Set up parsing of the command statement into its components.
+# INPUT: Takes in a command and parses it.
+# RETURN: False if the parsing fails, else the dictionary of the parsed command.
+# -----------------
 def parseCommand(command):
-	parser = argparse.ArgumentParser()
-	subparsers = parser.add_subparsers(dest="subcommand")
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(dest="subcommand")
 
-	# Used to display a specific day, month or year.
-	display = subparsers.add_parser("display")
-	display_group = display.add_mutually_exclusive_group()
-	display_group.add_argument("-m", nargs="?", const=datetime.datetime.now().strftime("%m-%Y"), type=str)
-	display_group.add_argument("-y", nargs="?", const=datetime.datetime.now().strftime("%Y"), type=str)
+    # This subcommand group will handle the displaying of months and years. 
+    display = subparsers.add_parser("display")
+    display_group = display.add_mutually_exclusive_group()
+    # display -m will display the calendar for a given month.
+    # display -m will default to the present day if no argumets are provided.
+    display_group.add_argument("-m", nargs="?", const=datetime.datetime.now().strftime("%m-%Y"), type=str)
+    # display -y will display the calendar for a given year.
+    # display -y will default to present year if no arguments are provided.
+    display_group.add_argument("-y", nargs="?", const=datetime.datetime.now().strftime("%Y"), type=str)
+        
+    # This subcommand group will handles various actions concerning events.
+    event = subparsers.add_parser("event")
+    event_group = event.add_mutually_exclusive_group()
+    # Used to add events to specific dates.
+    event_group.add_argument("-a", nargs=2)
+    # event -s will display all the events for a particular day.
+    # event -s will default to the present day if no argumets are provided.
+    event_group.add_argument("-s", nargs="?", const=datetime.datetime.now().strftime("%d-%m-%Y"), type=str)
 
-	# Used to add events to specific dates
-	event = subparsers.add_parser("event")
-	event_group = event.add_mutually_exclusive_group()
-	event_group.add_argument("-a", nargs=2)
-	event_group.add_argument("-s", nargs=1)
+    # This subcommand will close the application safely.
+    exit = subparsers.add_parser("exit")
 
-	# Used to close the application safely.
-	exit = subparsers.add_parser("exit")	
-	
-	try:
-		return vars(parser.parse_args(shlex.split(command)))
-	except:		
-		return False # If parsing fails, return false so that execCommand() can catch and quit.
+    # If parsing fails, return false so that execCommand() can catch and quit.
+    try:
+        return vars(parser.parse_args(shlex.split(command)))
+    except:
+        return False
 
-# Executes parsed commands.
+# --- FUNCTION ----
+# DESC: Executes parsed commands received by parseCommand() 
+# INPUT: Takes a parsed command dictionary and executes it.
+# RETURN: False if parseCommand returns False, True if the user inputs exit and None otherwise.
+# -----------------
 def execCommand(command):
-	global CMD_WIDTH, CMD_HEIGHT, EVENTS
+    global CMD_WIDTH, CMD_HEIGHT, EVENTS
 
-	# Catches failed command parsing from parseCommand()
-	if not command:
-		return False
-	
-	os.system("cls")
-	if command["subcommand"] == "display":
-		dispm = command["m"]
-		dispy = command["y"]
-		headerText = "Displaying "
+    # Catches failed command parsing from parseCommand()
+    if not command:
+        return False
 
-		if dispm is not None:
-			try:
-				date = list(map(int, dispm.split("-")))
-				if len(date) != 2:
-					renderError(CMD_WIDTH, CMD_HEIGHT, "You have either not provided either the month or year.")
-					return None
-			except:
-				renderError(CMD_WIDTH, CMD_HEIGHT, "Perhaps you did not use a '-' to seperate the month and year?")
-				return None
-			
-			month, year = date
-			if not 1 <= month <= 12:
-				renderError(CMD_WIDTH, CMD_HEIGHT, "Enter a month number between 1 and 12, inclusive.")
-				return None
-			elif year <= 0:
-				renderError(CMD_WIDTH, CMD_HEIGHT, "Please enter a positive integer year")
-				return None
+    os.system("cls")
+    if command["subcommand"] == "display":
+        dispm = command["m"]
+        dispy = command["y"]
+        headerText = "Displaying "
 
-			headerText += calendar.month_name[month] + " " + str(year)
-			monthCalendar = calendar.Calendar(0).monthdatescalendar(year, month)
-			renderMMMonth(addEventToMonthCalendar(monthCalendar, month), CMD_WIDTH, CMD_HEIGHT, headerText)
-		elif dispy is not None:
-			try:
-				year = int(dispy)
-			except:
-				renderError(CMD_WIDTH, CMD_HEIGHT, "Please enter valid numbers.")
-				return None
-			
-			if year <= 0:
-				renderError(CMD_WIDTH, CMD_HEIGHT, "Please enter a positive integer year.")
-				return None
+        if dispm is not None:
+            # Prevents alphabets, etc. by catching the int()'s exception
+            try:
+                date = list(map(int, dispm.split("-")))
 
-			headerText += str(year)
-			yearCalendar = numpy.reshape(calendar.Calendar(0).yeardayscalendar(year), (3, 4))
-			renderYMYear(yearCalendar, CMD_WIDTH, CMD_HEIGHT, headerText)
+                # Checks if there are two numbers for the month and year in the date.
+                if len(date) != 2:
+                    renderError(CMD_WIDTH, CMD_HEIGHT, "Please use display -m in the form mm-yyyy.")
+                    return None
+                month, year = date
 
-	elif command["subcommand"] == "event":
-		add = command["a"]
-		search = command["s"]
+                 # Checks if the day, month and year are in the right bounds.
+                if not (1 <= month <= 12 and year > 0):
+                    renderError(CMD_WIDTH, CMD_HEIGHT, "Enter a month number between 1 and 12, inclusive, and a positive integer year.")
+                    return None
+            except:
+                renderError(CMD_WIDTH, CMD_HEIGHT, "Perhaps you did not use a '-' to seperate the month and year?")
+                return None
+            
+            # Add the month and year to the header
+            # Then, get the dates for the specified month, runAddEventToMonthCalendar() (please refer above) and render.
+            headerText += calendar.month_name[month] + " " + str(year)
+            monthCalendar = calendar.Calendar(0).monthdatescalendar(year, month)
+            renderMMMonth(addEventToMonthCalendar(monthCalendar, month), CMD_WIDTH, CMD_HEIGHT, headerText)
 
-		if add is not None:
-			try:
-				date = list(map(int, add[0].split("-")))
-			except:
-				renderError(CMD_WIDTH, CMD_HEIGHT, "Perhaps you did not use a '-' to seperate the month and year?")
-				return None
-			
-			date = datetime.date(date[2], date[1], date[0])
-			event = add[1].strip()
-			if date in EVENTS.keys():
-				EVENTS[date].append(event)
-			else:
-				EVENTS[date] = [event]
-			with open("events.pkl", "wb") as f:
-				pickle.dump(EVENTS, f)
+        elif dispy is not None:
+            # Same code as dispm, please refer above.
+            try:
+                year = int(dispy)
+                if year <= 0:
+                    renderError(CMD_WIDTH, CMD_HEIGHT, "Please enter a positive integer year.")
+                    return None
+            except:
+                renderError(CMD_WIDTH, CMD_HEIGHT, "Please enter valid numbers.")
+                return None
+            
+            # Add the year to the header
+            # Then, get the dates for the specified year and render its calendar.
+            headerText += str(year)
+            yearCalendar = numpy.reshape(calendar.Calendar(0).yeardayscalendar(year), (3, 4))
+            renderYMYear(yearCalendar, CMD_WIDTH, CMD_HEIGHT, headerText)
 
-		elif search is not None:
-			date = list(map(int, search[0].split("-")))
-			date = datetime.date(date[2], date[1], date[0])
-			if date in EVENTS.keys():
-				monthCalendar = calendar.Calendar(0).monthdatescalendar(date.year, date.month)
-				renderMMMonth(addEventToMonthCalendar(monthCalendar, date.month), CMD_WIDTH, CMD_HEIGHT, "Search Results")
-				renderEventSearchBox(date, monthCalendar, EVENTS[date][:], CMD_WIDTH, CMD_HEIGHT)
-			else:
-				print("No events on this day.")
+    elif command["subcommand"] == "event":
+        add = command["a"]
+        search = command["s"]
 
-	elif command["subcommand"] == "exit":
-		return True
+        if add is not None:
+            # Same code as dispm, please refer above.
+            try:
+                date = list(map(int, add.split("-")))
+                if len(date) != 3:
+                    renderError(CMD_WIDTH, CMD_HEIGHT, "You have not provided a valid day, month or year.")
+                    return None
+                day, month, year = date
+                if not (1 <= day <= 31 and 1 <= month <= 12 and year > 0):
+                    renderError(CMD_WIDTH, CMD_HEIGHT, "Provides days between 0-32, months between 0-13 and positive years.")
+                    return None
+                date = datetime.date(year, month, day)
+            except:
+                renderError(CMD_WIDTH, CMD_HEIGHT, "Perhaps you did not use a '-' to seperate the month and year?")
+                return None
+            
+            # Check if the event string is empty.
+            if add[1]:
+                renderError(CMD_WIDTH, CMD_HEIGHT, "Please enter a non-blank event.")
+                return None
+            event = add[1].strip()
+
+            # Append to the event list for the specified date if it exists, else create a new list with this event.
+            # Then, save the updated dictionary of events to events.pkl.
+            if date in EVENTS.keys():
+                EVENTS[date].append(event)
+            else:
+                EVENTS[date] = [event]            
+            with open("events.pkl", "wb") as f:
+                pickle.dump(EVENTS, f)
+
+        elif search is not None:
+            # Same code as for dispm, please refer above. 
+            try:
+                date = list(map(int, search.split("-")))                
+                if len(date) != 3:
+                    renderError(CMD_WIDTH, CMD_HEIGHT, "You have not provided a valid day, month or year.")
+                    return None
+                day, month, year = date               
+                if not (1 <= day <= 31 and 1 <= month <= 12 and year > 0):
+                    renderError(CMD_WIDTH, CMD_HEIGHT, "Provides days between 0-32, months between 0-13 and positive years.")
+                    return None
+                date = datetime.date(year, month, day)
+            except:
+                renderError(CMD_WIDTH, CMD_HEIGHT, "Perhaps you did not use a '-' to seperate the day, month and year?")
+                return None
+            
+            # Checks if events exist in the specified day's events list.
+            if date in EVENTS.keys():
+                monthCalendar = calendar.Calendar(0).monthdatescalendar(date.year, date.month)
+                renderMMMonth(addEventToMonthCalendar(monthCalendar, date.month), CMD_WIDTH, CMD_HEIGHT, "Search Results")
+                renderEventSearchBox(date, monthCalendar, EVENTS[date][:], CMD_WIDTH, CMD_HEIGHT)
+            else:
+                renderError(CMD_WIDTH, CMD_HEIGHT, "No events were found on this day.")
+                return None
+
+    elif command["subcommand"] == "exit":
+        return True
+
+    return None
 
 
-# Init colorama so ANSI sequences are converted to Windows equivalents
+# ==== Driver Code ====
+
+# Init colorama so ANSI sequences can be used.
 colorama.init()
 
 # Get initial CMD dimensions and events from file
 CMD_WIDTH = os.get_terminal_size()[0]
 CMD_HEIGHT = os.get_terminal_size()[1]
 with open("events.pkl", "rb") as f:
-	EVENTS = pickle.load(f)
+    EVENTS = pickle.load(f)
 
 # Clear screen and render main page
 os.system("cls")
@@ -150,9 +224,11 @@ time.sleep(1)
 command = "display -m"
 execCommand(parseCommand(command))
 while True:
-	command = input("calendar>")
-	status = execCommand(parseCommand(command))
-	if status:
-		break
-	elif not status:
-		continue
+    command = input("calendar>")
+    status = execCommand(parseCommand(command))
+    if status:
+        break
+    elif not status:
+        continue
+    
+# =====================
